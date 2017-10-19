6bf06eb45e0587f192726fb0710964b06b04db9c
Fabrice René Schindler <fabrice@zyria.de>
2017-10-19 16:48:20 +0200

    some modifications

--- install.sh
+++ install.sh
@@ -43,14 +43,14 @@ mdadm -S --scan
 # Partition setup
 # Delte them all
 # TODO There may be more than 5 partitions
-for disk in `lsblk -d -o NAME -n grep sd`; do
+for disk in `lsblk -d -o NAME -n | grep sd`; do
   sgdisk -Z $disk 
 done
 wait_for_key
 ###############################################################################
 
 
-for d in `lsblk -d -o NAME -n grep sd`; do
+for d in `lsblk -d -o NAME -n | grep sd`; do
   parted -a optimal /dev/$d --script \
     unit s \
     mklabel gpt \
@@ -129,74 +129,108 @@ mount -v /dev/vg-`hostname`/usr /target/usr/
 mount -v /dev/vg-`hostname`/var /target/var/
 mkdir -pv /target/var/log
 mount -v /dev/vg-`hostname`/var-log /target/var/log/
-mount -v --rbind /proc/ /target/proc/
-mount -v --rbind /dev/ /target/dev/
-mount -v --rbind /sys/ /target/sys/
-mount -v --rbind /dev/pts /target/dev/pts
 mount -v /dev/md0 /target/boot
 wait_for_key
 ###############################################################################
 
-# download and install debootstrap
-h "installing debootstrap"
-wget http://archive.ubuntu.com/ubuntu/pool/main/d/debootstrap/debootstrap_${DEBOOTSTRAP_VERSION}_all.deb
-dpkg -i debootstrap_${DEBOOTSTRAP_VERSION}_all.deb
-rm debootstrap_*.deb
-wait_for_key
-
 h "running debootstrap"
-debootstrap --arch=amd64 --components=main,restricted,universe,multiverse --verbose ${UBUNTU_VERSION} /newroot http://archive.ubuntu.com/ubuntu/
+debootstrap \
+  --components=main,contrib,non-free \
+  --verbose ${DEBIAN_VERSION} \
+  /target \
+  http://deb.debian.org/debian/
 wait_for_key
+###############################################################################
 
 h "writing fstab"
+
+###############################################################################
+
 cat >/newroot/etc/fstab <<EOF
+
 # <file system> <mount point>   <type>  <options>       <dump>  <pass>
-proc            /proc           proc    defaults  0 0
+# Boot
+/dev/md0 /boot ext4 rw,noatime,discard,data=ordered,commit=600 0 1
+
+# Sys
+proc /proc proc defaults 0 0
+#sysfs /sys sysfs nosuid,nodev,noexec 0 0
 none            /dev/pts        devpts  gid=5,mode=620 0 0
-#sys             /sys            sysfs   nodev,noexec,nosuid 0 0
-/dev/vg0/rootfs /               ext4    defaults            0 0
-/dev/md/0       /boot           ext4    defaults            0 1
-/dev/vg0/swap   none            swap    sw                  0 0
+
+# LVM
+/dev/mapper/vg--$(hostname)-swap none swap  sw,discard 0  0
+/dev/mapper/vg--$(hostname)-root / ext4 rw,noatime,discard,data=ordered 0 1
+/dev/mapper/vg--$(hostname)-usr /usr ext4 rw,noatime,discard,data=ordered 0 1
+/dev/mapper/vg--$(hostname)-home /home ext4 rw,noatime,discard,data=ordered,noexec,nodev,nosuid 0 1
+/dev/mapper/vg--$(hostname)-tmp /tmp ext4 rw,noatime,discard,data=ordered,noexec,nodev,nosuid 0 1
+/dev/mapper/vg--$(hostname)-var /var ext4 rw,noatime,discard,acl,user_xattr,barrier=1,data=ordered,usrquota,grpquota 0 1
+/dev/mapper/vg--$(hostname)-var--log /var/log ext4 rw,noatime,discard,acl,user_xattr,barrier=1,data=ordered 0 1
+
+# Network
+nfs.hetzner.de:/nfs /mnt/hetzner_nfs nfs ro 0 0
+//u129418.your-storagebox.de/backup  /mnt/hetzner_backup cifs uid=0,gid=0,_netdev,credentials=/root/.smbcredentials.u129418 0 0
+//u130753.your-backup.de/backup  /mnt/hetzner_$(hostname) cifs uid=0,gid=0,_netdev,credentials=/root/.smbcredentials.u130753 0 0
+
+# Rebind
+/tmp /var/tmp none bind
 EOF
-chroot /newroot /bin/bash -c "grep -v swap /etc/fstab >/etc/mtab"
+###############################################################################
+
+chroot /target /bin/bash -c "grep -v swap /etc/fstab >/etc/mtab"
 wait_for_key
 
 h "mounting filesystems - stage 2"
-mount -v --rbind /dev /newroot/dev
-#mount -v --rbind /dev/pts /newroot/dev/pts
-mount -v --rbind /proc /newroot/proc
-mount -v --rbind /sys /newroot/sys
-chroot /newroot locale-gen en_US.UTF-8
-chroot /newroot update-locale LANG=en_US.UTF-8
+mount -v --rbind /proc/ /target/proc/
+mount -v --rbind /dev/ /target/dev/
+mount -v --rbind /sys/ /target/sys/
+#mount -v --rbind /dev/pts /target/dev/pts
+
+###############################################################################
+chroot /target locale-gen de_DE.UTF-8
+chroot /target update-locale LANG=de_DE.UTF-8
 wait_for_key
 
-chroot /newroot dpkg-reconfigure tzdata
+###############################################################################
+chroot /target dpkg-reconfigure tzdata
 
+###############################################################################
 h "configure networking"
-# TODO
-cat >/newroot/etc/network/interfaces <<EOF
-# Loopback device:
+
+cat >/target/etc/network/interfaces <<EOF
+# cat /etc/network/interfaces
+# interfaces(5) file used by ifup(8) and ifdown(8)
+# Include files from /etc/network/interfaces.d:
+source-directory /etc/network/interfaces.d
+
 auto lo
 iface lo inet loopback
 
-## device: eth0
+# device: eth0
 auto eth0
 iface eth0 inet static
-  address ${TARGET_IPADDR}
-  netmask ${TARGET_NETMASK}
-  gateway ${TARGET_GATEWAY}
-#  post-up mii-tool -F 100baseTx-FD eth0
+dns-nameservers 213.133.98.96 213.133.99.99 213.133.98.98 213.133.100.100
+address ${TARGET_IPADDR}
+netmask ${TARGET_NETMASK}
+gateway ${TARGET_GATEWAY}
+pointopoint ${TARGET_GATEWAY}
+#up ip addr add 176.9.67.11/32  dev eth0
+#down ip addr del 176.9.67.11/32 dev eth0
+
+#iface eth0 inet6 static
+#address 2a01:4f8:151:820a::2
+#netmask 128
+#gateway fe80::1
+#privext 2
+#down ip addr del 2a01:4f8:151:820a::53/128 dev eth0
+#up ip addr add 2a01:4f8:151:820a::53/128 dev eth0
+
 EOF
 
-# TODO
-cat >/newroot/etc/resolvconf/resolv.conf.d/original <<EOF
-search ${TARGET_DOMAIN}
-nameserver 8.8.8.8
-nameserver 213.133.98.98
-EOF
+###############################################################################
+cp /etc/resolv.conf /target/etc/resolv.conf
+sed -i -e 's/your-server.de/'${TARGET_DOMAIN}'/g' /target/etc/resolv.conf
 
-# TODO
-cat >/newroot/etc/hosts <<EOF
+cat >/target/etc/hosts <<EOF
 127.0.0.1	localhost
 ${TARGET_IPADDR} ${TARGET_HOSTNAME}.${TARGET_DOMAIN} ${TARGET_HOSTNAME}
 
@@ -208,101 +242,75 @@ ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
 EOF
 
-# TODO
-echo "${TARGET_HOSTNAME}" >/newroot/etc/hostname
+echo "${TARGET_HOSTNAME}" >/target/etc/hostname
 wait_for_key
+
+###############################################################################
 
 h "setting up apt"
 # install missing packages
+
 cp -f /newroot/etc/apt/sources.list /newroot/etc/apt/sources.list.orig
-cat >/newroot/etc/apt/sources.list <<EOF
-deb http://archive.ubuntu.com/ubuntu/ ${UBUNTU_VERSION} main restricted universe multiverse 
-#deb-src http://archive.ubuntu.com/ubuntu/ ${UBUNTU_VERSION} main restricted universe multiverse 
-deb http://archive.ubuntu.com/ubuntu/ ${UBUNTU_VERSION}-updates main restricted universe multiverse 
-#deb-src http://archive.ubuntu.com/ubuntu/ ${UBUNTU_VERSION}-updates main restricted universe multiverse 
-#deb http://archive.ubuntu.com/ubuntu/ ${UBUNTU_VERSION}-backports main restricted universe multiverse 
-#deb-src http://archive.ubuntu.com/ubuntu/ ${UBUNTU_VERSION}-backports main restricted universe multiverse 
-deb http://security.ubuntu.com/ubuntu ${UBUNTU_VERSION}-security main restricted universe multiverse 
-#deb-src http://security.ubuntu.com/ubuntu ${UBUNTU_VERSION}-security main restricted universe multiverse 
+cat <<EOF > /target/etc/apt/sources.list
+
+# Packages from the Hetzner Debian Mirror
+#deb ftp://mirror.hetzner.de/debian/packages ${DEBIAN_VERSION} main contrib non-free
+deb ftp://mirror.hetzner.de/debian/packages ${DEBIAN_VERSION} main contrib non-free
+deb ftp://mirror.hetzner.de/debian/security ${DEBIAN_VERSIION}/updates main contrib non-free
+deb ftp://ftp.de.debian.org/debian/ ${DEBIAN_VERSION} main contrib non-free
+deb http://security.debian.org/ ${DEBIAN_VERSION}/updates main contrib non-free
+
+deb http://ftp.de.debian.org/debian/ ${DEBIAN_VERSION}-backports main contrib non-free
+deb http://ftp.de.debian.org/debian ${DEBIAN_VERSION}-proposed-updates main contrib non-free
 EOF
 wait_for_key
+###############################################################################
 
 h "update package index and install missing packages"
-chroot /newroot apt-get -y update
-chroot /newroot apt-get -y install openssh-server lvm2 mdadm initramfs-tools
-chroot /newroot /bin/bash -c "/usr/share/mdadm/mkconf > /etc/mdadm/mdadm.conf"
+chroot /target apt-get -y update
+chroot /target apt-get -y install openssh-server lvm2 mdadm initramfs-tools
+chroot /target /bin/bash -c "/usr/share/mdadm/mkconf > /etc/mdadm/mdadm.conf"
+chroot /target apt-get -y install console-common,manpages-de,ifupdown,cryptsetup,\
+  manpages-dev,sudo,vim,console-data,salt-minion,htop,aptitude,rkhunter,glances,\
+  git,busybox,openssh-blacklist,manpages-posix-dev,dropbear-initramfs,salt-master,\
+  apt-listchanges,logcheck,hashalot,john,firmware-realtek,debsecan,manpages-de-dev,\
+  chkrootkit,bzip2,bash-completion,task-german,keyboard-configuration,most,\
+  debootstrap,less,exim4-daemon-light,sensord,etckeeper,locales,manpages-posix,\
+  iotop,smartmontools,iftop,intel-microcode,deborphan,command-not-found,nfs-common,\
+  pciutils,pv,htop,radvd,tmux,fail2ban,python-gamin,debian-security-support,\
+  dnsutils,console-setup,ebtables,parted
+
+chroot /target /bin/bash -c "dpkg-reconfigure -plow unattended-upgrades"
+
 wait_for_key
+###############################################################################
 
 h "install kernel and bootloader"
-chroot /newroot apt-get -y install linux-generic
-chroot /newroot apt-get -y install grub-pc
-chroot /newroot /bin/bash -c "update-initramfs -k all -u"
-#chroot /newroot /bin/bash -c 'echo -e "device (hd0) /dev/sda\nroot (hd0,0)\nsetup (hd0)\nquit"|grub --batch'
-# TODO
-cat >/newroot/etc/default/grub <<EOF
-# If you change this file, run 'update-grub' afterwards to update
-# /boot/grub/grub.cfg.
-# For full documentation of the options in this file, see:
-#   info -f grub -n 'Simple configuration'
-
-GRUB_DEFAULT=0
-#GRUB_HIDDEN_TIMEOUT=0
-GRUB_HIDDEN_TIMEOUT_QUIET=true
-GRUB_TIMEOUT=5
-GRUB_DISTRIBUTOR=\`lsb_release -i -s 2> /dev/null || echo Debian\`
-GRUB_CMDLINE_LINUX_DEFAULT="nomodeset"
-GRUB_CMDLINE_LINUX="bootdegraded=true"
-
-# Uncomment to enable BadRAM filtering, modify to suit your needs
-# This works with Linux (no patch required) and with any kernel that obtains
-# the memory map information from GRUB (GNU Mach, kernel of FreeBSD ...)
-#GRUB_BADRAM="0x01234567,0xfefefefe,0x89abcdef,0xefefefef"
-
-# Uncomment to disable graphical terminal (grub-pc only)
-GRUB_TERMINAL=console
-
-# The resolution used on graphical terminal
-# note that you can use only modes which your graphic card supports via VBE
-# you can see them in real GRUB with the command \`vbeinfo'
-#GRUB_GFXMODE=640x480
-
-# Uncomment if you don't want GRUB to pass "root=UUID=xxx" parameter to Linux
-#GRUB_DISABLE_LINUX_UUID=true
-
-# Uncomment to disable generation of recovery mode menu entries
-#GRUB_DISABLE_RECOVERY="true"
-
-# Uncomment to get a beep at grub start
-#GRUB_INIT_TUNE="480 440 1"
-
-GRUB_VIDEO_BACKEND="vga"
-GRUB_GFXPAYLOAD_LINUX="text"
-EOF
-chroot /newroot /bin/bash -c "grub-install --no-floppy --recheck /dev/sda"
-chroot /newroot /bin/bash -c "grub-install --no-floppy --recheck /dev/sdb"
-chroot /newroot /bin/bash -c "update-grub"
+chroot /target apt-get -y install linux-image-amd64
+chroot /target apt-get -y install grub2
+chroot /target /bin/bash -c "update-initramfs -k all -u"
+for disk in `lsblk -d -o NAME -n | grep sd`; do
+  chroot /target /bin/bash -c "grub-install --no-floppy --recheck /dev/$disk"
+done
+chroot /target /bin/bash -c "update-grub2"
 wait_for_key
+###############################################################################
+
 
 h "installing authorized_keys"
 mkdir -m 0700 /newroot/root/.ssh
-# TODO
 cat >/newroot/root/.ssh/authorized_keys <<EOF
 ${SSH_KEY}
 EOF
 
 h "setting root password"
 chroot /newroot passwd -S root
-# TODO
 cat <<EOF | chroot /newroot passwd root
 ${INITIAL_ROOT_PASSWORD}
 ${INITIAL_ROOT_PASSWORD}
 EOF
 wait_for_key
 
-echo "now reboot into the new machine and exec:"
-echo "  apt-get install ubuntu-standard tasksel"
-echo "  tasksel install server"
-echo "  dpkg-reconfigure postfix"
-echo "etc..."
+echo "now reboot into the new machine "
 
 exit
